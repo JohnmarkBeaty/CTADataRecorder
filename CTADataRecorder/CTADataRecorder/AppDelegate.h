718a1d5ea89fc6e1a698552cfc4adae4465ef055
Johnmark Beaty <johnmarkbeaty@gmai.com>
2014-07-23 02:35:40 -0500

    First Commit
    first commit

---
+++ CTADataRecorder/CTADataRecorder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CTADataRecorder
+//
+//  Created by Johnmark Beaty on 7/23/14.
+//  Copyright (c) 2014 JohnmarkBeaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CTADataRecorder/CTADataRecorder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CTADataRecorder
+//
+//  Created by Johnmark Beaty on 7/23/14.
+//  Copyright (c) 2014 JohnmarkBeaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
